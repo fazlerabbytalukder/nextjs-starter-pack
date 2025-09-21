9235d5a8fed2c2de0889abf2f1bdf7d6454b794b
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2025-09-21 18:28:28 +0600

    apply custom font here

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="custom-container">
-      <p className="text-base sm:text-3xl font-bold text-primary">
+      <p className="text-base sm:text-3xl font-bold text-primary font-payfair">
         Next js starter pack
       </p>
       <Link href="/auth/login" className="text-secondary">
