b1051a30470ff0e8ad2fea26f756451dcf87ac8d
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2025-09-24 11:34:55 +0600

    register page added

--- components/auth/Register.jsx
+++ components/auth/Register.jsx
@@ -1,7 +1,174 @@
+import Link from "next/link";
+
 const Register = () => {
   return (
     <div>
-      <p>Register</p>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-8">
+          <h1 className="text-3xl font-bold mb-5">Register</h1>
+          <form>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="first_name"
+              >
+                First Name <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="first_name"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="last_name"
+              >
+                Last Name <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="last_name"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="text"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="phone"
+              >
+                Phone <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone"
+                type="text"
+                placeholder="Phone"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="country"
+              >
+                Country <span className="text-red-500">*</span>
+              </label>
+              <select
+                className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="country"
+              >
+                <option value="">Select Country</option>
+                <option value="india">India</option>
+                <option value="usa">USA</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="gender"
+              >
+                Gender <span className="text-red-500">*</span>
+              </label>
+              <select
+                className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="gender"
+              >
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password_confirmation"
+              >
+                Confirm Password <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password_confirmation"
+                type="password"
+                placeholder="Confirm Password"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="inline-flex items-center">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 text-primary"
+                />
+                <span className="ml-2 text-gray-700">
+                  Agree terms and conditions
+                </span>
+              </label>
+            </div>
+            <div className="flex justify-center">
+              <button
+                className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+            <div className="mt-4 text-center">
+              <p className="text-gray-700">
+                Already have an account?{" "}
+                <Link href="/auth/login" className="text-primary">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
